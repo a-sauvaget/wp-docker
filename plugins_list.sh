f438e6a91247795f3a08036c4bea305702bb32e5
Alexandre Sauvaget <70508923+a-sauvaget@users.noreply.github.com>
2025-02-25 16:54:36 +0100

    [chore] ajout de debug this à la liste des plugins

--- plugins_list.sh
+++ plugins_list.sh
@@ -2,5 +2,6 @@
 
 # Installer et activer des plugins
 wp plugin install debug-bar --activate
+wp plugin install debug-this --activate
 wp plugin install query-monitor --activate
 wp plugin install theme-check --activate
